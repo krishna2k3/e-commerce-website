5fdd5b23e362c42da615904c2b799d607020b137
krishna2k3 <krishna101ravi@gmail.com>
2023-07-13 12:00:02 +0530

    redis config now accepts env configs

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,9 +1,13 @@
 import { Redis } from "@upstash/redis";
+import dotenv from "dotenv";
 import "isomorphic-fetch";
+
+dotenv.config();
 
 const redis = new Redis({
   url: process.env.REDIS_URL,
-  token: process.env.REDIS_TOKEN,
+  token:
+    process.env.REDIS_TOKEN,
 });
 
 const data = await redis.set("foo", "bar");
